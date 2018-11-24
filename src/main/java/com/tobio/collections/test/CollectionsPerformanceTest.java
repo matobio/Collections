2ecc75dac32e2eb1339df76f8fe7ed1d00ed82e7
Manuel Tobio <Manuel Tobio@ManuelTobio-PC>
2018-11-24 14:50:43 +0100

    asd

--- src/main/java/com/tobio/collections/test/CollectionsPerformanceTest.java
+++ src/main/java/com/tobio/collections/test/CollectionsPerformanceTest.java
@@ -1,6 +1,7 @@
 package com.tobio.collections.test;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Vector;
 import java.util.concurrent.TimeUnit;
@@ -9,9 +10,11 @@ public class CollectionsPerformanceTest {
 
     public static void main(String[] args) {
 
-        performanceTest(100000);
-        performanceTest(1000000);
-        performanceTest(100000000);
+        int samples = 100000;
+        // performanceTest(samples);
+        // performanceTest(samples * 5);
+        // performanceTest(samples * 10);
+        performanceTest(samples * 100);
 
         System.exit(0);
     }
@@ -22,25 +25,65 @@ public class CollectionsPerformanceTest {
         System.out.println(">>>> Samples:  " + samples);
 
         ArrayList<Integer> arrayList = new ArrayList<>();
-        LinkedList<Integer> linkedList = new LinkedList<Integer>();
-        Vector<Integer> vectorList = new Vector<Integer>();
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        Vector<Integer> vectorList = new Vector<>();
+        HashSet<Integer> hashSet = new HashSet<>();
 
-        operationAdd(samples, arrayList, linkedList, vectorList);
+        operationAdd(samples, arrayList, linkedList, vectorList, hashSet);
 
-        operationGet(samples, arrayList, linkedList, vectorList);
+        operationGet(samples, arrayList, linkedList, vectorList, hashSet);
 
-        operationRemove(samples, arrayList, linkedList, vectorList);
+        operationRemove(samples, arrayList, linkedList, vectorList, hashSet);
 
     }
 
 
-    private static void operationRemove(int samples, ArrayList<Integer> arrayList, LinkedList<Integer> linkedList, Vector<Integer> vectorList) {
+    private static void operationAdd(int samples, ArrayList<Integer> arrayList, LinkedList<Integer> linkedList, Vector<Integer> vectorList, HashSet<Integer> hashSet) {
+
+        System.err.println(">> Operation <add>:  ");
+        // arrayListAdd(samples, arrayList);
+
+        // linkedListAdd(samples, linkedList);
+
+        // vectorAdd(samples, vectorList);
+
+        hashSetAdd(samples, hashSet);
+
+    }
+
+
+    private static void operationGet(int samples, ArrayList<Integer> arrayList, LinkedList<Integer> linkedList, Vector<Integer> vectorList, HashSet<Integer> hashSet) {
+
+        System.err.println(">> Operation <get>:  ");
+
+        arrayListGet(samples, arrayList);
+
+        // linkedListGet(samples, linkedList);
+
+        // vectorGet(samples, vectorList);
+    }
+
+
+    private static void operationRemove(int samples, ArrayList<Integer> arrayList, LinkedList<Integer> linkedList, Vector<Integer> vectorList, HashSet<Integer> hashSet) {
+
+        System.err.println(">> Operation <remove>:  ");
+        // arrayListRemove(samples, arrayList);
+
+        // linkedListRemove(samples, linkedList);
+
+        // vectorRemove(samples, vectorList);
+
+        hashSetRemove(samples, hashSet);
+    }
+
+
+    private static void arrayListRemove(int samples, ArrayList<Integer> arrayList) {
         double elapsedTimeInSeconds;
         long startTime;
         long endTime;
         long duration;
         // ArrayList remove
-        System.out.println(">> Operation <remove>:  ");
+
         startTime = System.nanoTime();
 
         for (int i = samples - 1; i >= 0; i--) {
@@ -49,8 +92,34 @@ public class CollectionsPerformanceTest {
         endTime = System.nanoTime();
         duration = endTime - startTime;
         elapsedTimeInSeconds = TimeUnit.MILLISECONDS.convert(duration, TimeUnit.NANOSECONDS) / 1000.0;
-        printResult("ArrayList", elapsedTimeInSeconds, duration);
+        printResult("ArrayList remove", elapsedTimeInSeconds, duration);
+    }
 
+
+    private static void hashSetRemove(int samples, HashSet<Integer> hashSet) {
+        double elapsedTimeInSeconds;
+        long startTime;
+        long endTime;
+        long duration;
+        // ArrayList remove
+
+        startTime = System.nanoTime();
+
+        for (int i = samples - 1; i >= 0; i--) {
+            hashSet.remove(i);
+        }
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        elapsedTimeInSeconds = TimeUnit.MILLISECONDS.convert(duration, TimeUnit.NANOSECONDS) / 1000.0;
+        printResult("ArrayList remove", elapsedTimeInSeconds, duration);
+    }
+
+
+    private static void linkedListRemove(int samples, LinkedList<Integer> linkedList) {
+        double elapsedTimeInSeconds;
+        long startTime;
+        long endTime;
+        long duration;
         // LinkedList remove
         startTime = System.nanoTime();
 
@@ -60,8 +129,15 @@ public class CollectionsPerformanceTest {
         endTime = System.nanoTime();
         duration = endTime - startTime;
         elapsedTimeInSeconds = TimeUnit.MILLISECONDS.convert(duration, TimeUnit.NANOSECONDS) / 1000.0;
-        printResult("LinkedList", elapsedTimeInSeconds, duration);
+        printResult("LinkedList remove", elapsedTimeInSeconds, duration);
+    }
 
+
+    private static void vectorRemove(int samples, Vector<Integer> vectorList) {
+        double elapsedTimeInSeconds;
+        long startTime;
+        long endTime;
+        long duration;
         // Vector remove
         startTime = System.nanoTime();
 
@@ -71,22 +147,22 @@ public class CollectionsPerformanceTest {
         endTime = System.nanoTime();
         duration = endTime - startTime;
         elapsedTimeInSeconds = TimeUnit.MILLISECONDS.convert(duration, TimeUnit.NANOSECONDS) / 1000.0;
-        printResult("Vector", elapsedTimeInSeconds, duration);
+        printResult("Vector remove", elapsedTimeInSeconds, duration);
     }
 
 
     private static void printResult(String type, double elapsedTimeInSeconds, long duration) {
-        System.out.println(type + ":  " + duration + "ns, " + elapsedTimeInSeconds + "ms");
+        System.out.println(type + ":  " + duration + ", " + elapsedTimeInSeconds + "ms");
     }
 
 
-    private static void operationGet(int samples, ArrayList<Integer> arrayList, LinkedList<Integer> linkedList, Vector<Integer> vectorList) {
+    private static void arrayListGet(int samples, ArrayList<Integer> arrayList) {
         double elapsedTimeInSeconds;
         long startTime;
         long endTime;
         long duration;
         // ArrayList get
-        System.out.println(">> Operation <get>:  ");
+
         startTime = System.nanoTime();
 
         for (int i = 0; i < samples; i++) {
@@ -95,8 +171,15 @@ public class CollectionsPerformanceTest {
         endTime = System.nanoTime();
         duration = endTime - startTime;
         elapsedTimeInSeconds = TimeUnit.MILLISECONDS.convert(duration, TimeUnit.NANOSECONDS) / 1000.0;
-        printResult("ArrayList", elapsedTimeInSeconds, duration);
+        printResult("ArrayList get", elapsedTimeInSeconds, duration);
+    }
 
+
+    private static void linkedListGet(int samples, LinkedList<Integer> linkedList) {
+        double elapsedTimeInSeconds;
+        long startTime;
+        long endTime;
+        long duration;
         // LinkedList get
         startTime = System.nanoTime();
 
@@ -106,8 +189,15 @@ public class CollectionsPerformanceTest {
         endTime = System.nanoTime();
         duration = endTime - startTime;
         elapsedTimeInSeconds = TimeUnit.MILLISECONDS.convert(duration, TimeUnit.NANOSECONDS) / 1000.0;
-        printResult("LinkedList", elapsedTimeInSeconds, duration);
+        printResult("LinkedList get", elapsedTimeInSeconds, duration);
+    }
 
+
+    private static void vectorGet(int samples, Vector<Integer> vectorList) {
+        double elapsedTimeInSeconds;
+        long startTime;
+        long endTime;
+        long duration;
         // Vector get
         startTime = System.nanoTime();
 
@@ -117,14 +207,13 @@ public class CollectionsPerformanceTest {
         endTime = System.nanoTime();
         duration = endTime - startTime;
         elapsedTimeInSeconds = TimeUnit.MILLISECONDS.convert(duration, TimeUnit.NANOSECONDS) / 1000.0;
-        printResult("Vector", elapsedTimeInSeconds, duration);
+        printResult("Vector get", elapsedTimeInSeconds, duration);
     }
 
 
-    private static void operationAdd(int samples, ArrayList<Integer> arrayList, LinkedList<Integer> linkedList, Vector<Integer> vectorList) {
+    private static void arrayListAdd(int samples, ArrayList<Integer> arrayList) {
         double elapsedTimeInSeconds;
-        // ArrayList add
-        System.out.println(">> Operation <add>:  ");
+
         long startTime = System.nanoTime();
 
         for (int i = 0; i < samples; i++) {
@@ -133,20 +222,30 @@ public class CollectionsPerformanceTest {
         long endTime = System.nanoTime();
         long duration = endTime - startTime;
         elapsedTimeInSeconds = TimeUnit.MILLISECONDS.convert(duration, TimeUnit.NANOSECONDS) / 1000.0;
-        printResult("ArrayList", elapsedTimeInSeconds, duration);
+        printResult("ArrayList add", elapsedTimeInSeconds, duration);
+    }
 
-        // LinkedList add
-        startTime = System.nanoTime();
+
+    private static void hashSetAdd(int samples, HashSet<Integer> hashSet) {
+        double elapsedTimeInSeconds;
+
+        long startTime = System.nanoTime();
 
         for (int i = 0; i < samples; i++) {
-            linkedList.add(i);
+            hashSet.add(i);
         }
-        endTime = System.nanoTime();
-        duration = endTime - startTime;
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
         elapsedTimeInSeconds = TimeUnit.MILLISECONDS.convert(duration, TimeUnit.NANOSECONDS) / 1000.0;
-        printResult("LinkedList", elapsedTimeInSeconds, duration);
+        printResult("HashSet add", elapsedTimeInSeconds, duration);
+    }
 
-        // Vector add
+
+    private static void vectorAdd(int samples, Vector<Integer> vectorList) {
+        double elapsedTimeInSeconds;
+        long startTime;
+        long endTime;
+        long duration;
         startTime = System.nanoTime();
 
         for (int i = 0; i < samples; i++) {
@@ -155,6 +254,23 @@ public class CollectionsPerformanceTest {
         endTime = System.nanoTime();
         duration = endTime - startTime;
         elapsedTimeInSeconds = TimeUnit.MILLISECONDS.convert(duration, TimeUnit.NANOSECONDS) / 1000.0;
-        printResult("Vector", elapsedTimeInSeconds, duration);
+        printResult("Vector add", elapsedTimeInSeconds, duration);
+    }
+
+
+    private static void linkedListAdd(int samples, LinkedList<Integer> linkedList) {
+        double elapsedTimeInSeconds;
+        long startTime;
+        long endTime;
+        long duration;
+        startTime = System.nanoTime();
+
+        for (int i = 0; i < samples; i++) {
+            linkedList.add(i);
+        }
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        elapsedTimeInSeconds = TimeUnit.MILLISECONDS.convert(duration, TimeUnit.NANOSECONDS) / 1000.0;
+        printResult("LinkedList add", elapsedTimeInSeconds, duration);
     }
 }
